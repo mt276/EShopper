aeb96177afb1d6559ccc99d3b7e8f7a7958befeb
Dương Minh Triều <trieu7458@gmail.com>
2024-06-11 21:45:37 +0700

    update

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [StringLength(150)]
+        public string? CategoryName { get; set; }
+        [StringLength(300)]
+        public string? CategoryDescription { get; set; }
+        [StringLength(300)]
+        public string? CategoryPhoto { get; set; }
+        public int CategoryOrder { get; set; }
+
+    }
+}
---
+++ Models/Size.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Models
+{
+    public class Size
+    {
+        [Key]
+        public int SizeID { get; set; }
+        [StringLength(10)]
+        public string? SizeName { get; set; }
+    }
+}
