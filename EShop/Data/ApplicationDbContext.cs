a1ad96e98bc7117e8c27d6ddafdd7a550df7a5c0
Dương Minh Triều <trieu7458@gmail.com>
2024-06-11 21:47:49 +0700

    update

---
+++ EShop/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EShop.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShop.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Size> Sizes { get; set; }
+        public DbSet<Color> Colors { get; set; }
+    }
+}
---
+++ EShop/Models/Color.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EShop.Models
+{
+    public class Color
+    {
+        [Key]
+        public int ColorID { get; set; }
+        [StringLength(150)]
+        public string? ColorName { get; set; }
+    }
+}
